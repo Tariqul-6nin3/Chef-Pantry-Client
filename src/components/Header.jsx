7c510b6f3db6cd1ce63b9f2b2853bb3bdce38e81
Tariqul <tariqularmsfitt018@gmail.com>
2023-05-04 11:57:55 +0600

    Tooltip of displayname added to userphoto on mousehover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import { Link, NavLink } from "react-router-dom";
 import { myContext } from "../providers/Context";
 import "../components/styles/root.css";
 import { FaBars, FaCross } from "react-icons/fa";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
 
 const Header = () => {
   const { user, logOut } = useContext(myContext);
@@ -14,27 +16,27 @@ const Header = () => {
       .catch(error => {});
   };
   return (
-    <div className="navbar bg-gray-50 fixed z-40">
+    <div className="navbar md:py-4 bg-gray-50 fixed z-40">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost normal-case">
-          <span className="logo">Chef</span> <span className="span">Eye</span>
+          <span className="text-3xl font-serif font-semibold">ChefPantry</span>
         </Link>
       </div>
       <div className="font-bold md:hidden">
-        <button onClick={() => setOpen(!open)}>
+        <button className="mr-5" onClick={() => setOpen(!open)}>
           {open ? (
-            <FaBars className="h-12 w-12" />
+            <FaBars className="h-10 w-10" />
           ) : (
-            <FaCross className="h-12 w-12" />
+            <FaCross className="h-10 w-10" />
           )}
         </button>
       </div>
-      <div className="flex-none gap-2">
+      <div className="flex-none   gap-2">
         <div
-          className={`md:mr-44 text-2xl font-semibold md:text-xl list-none md:flex md:space-x-7 ${
+          className={`md:mr-20 text-2xl font-semibold md:text-xl list-none md:flex md:space-x-12 ${
             open
-              ? "absolute -top-60 duration-700"
-              : "absolute top-16 bg-black w-full text-center  flex left-0 h-36 items-center  flex-col "
+              ? "absolute -top-40 md:top-6 right-0"
+              : "absolute top-16 bg-orange-100 w-full text-center  flex gap-3 left-0 h-48 pt-3 pb-4 items-center  flex-col "
           }`}>
           <li>
             <NavLink
@@ -55,6 +57,7 @@ const Header = () => {
               Blogs
             </NavLink>
           </li>
+
           {user ? (
             <>
               <li>
@@ -69,10 +72,15 @@ const Header = () => {
                   SignOut
                 </NavLink>
               </li>
-              <div className="">
+              <div className="mb-2">
                 <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                  <div className="w-10 rounded-full">
-                    <img src={user.photoURL} />
+                  <div
+                    className="w-10 rounded-full"
+                    data-tooltip-id="my-tooltip"
+                    data-tooltip-content={user.displayName}
+                    data-tooltip-place="left">
+                    <Tooltip id="my-tooltip"></Tooltip>
+                    <img src={user.photoURL} alt={user.displayName} />
                   </div>
                 </label>
               </div>
--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -5,24 +5,24 @@ import { FaVideo } from "react-icons/fa";
 
 const Lesson = () => {
   return (
-    <div className="lesson-info rounded-xl mb-16 my-container md:grid grid-cols-1 md:grid-cols-2">
-      <div className="">
-        <h2 className="md:text-6xl text-4xl text-center md:text-left font-bold md:tracking-wide md:leading-6">
-          Fun and Easy <br /> to Become <br /> a Master Chef
+    <div className="lesson-info rounded-xl mb-16 my-container md:grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="px-4">
+        <h2 className="md:text-5xl text-4xl text-left font-bold ">
+          Fun and Easy to Become a Master Chef
         </h2>
-        <p className="text-2xl mt-12 pe-8 ">
+        <p className="text-2xl mt-6 md:mt-12 pe-8 ">
           Let's join us to become a cooking expert in every kind of cuisine with
           a fun way to learn
         </p>
-        <div className="flex flex-col-reverse md:flex-row mt-12 justify-center gap-6 md:gap-24">
+        <div className="flex flex-col-reverse md:flex-row mt-7 md:mt-12 justify-center md:gap-20">
           <button className="btn-secondary">Join Lessons</button>
-          <button className="btn btn-outline mt-3">
+          <button className="btn btn-outline ">
             <FaVideo className="text-2xl mr-4 "></FaVideo> Watch Our Videos
           </button>
         </div>
       </div>
-      <div className="">
-        <img src={chef} alt="" />
+      <div className="mt-7 md:mt-0">
+        <img className="w-full h-full" src={chef} alt="" />
       </div>
     </div>
   );
